0d409d32fb495203b6de81ad2029a025c13022ca
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-01 02:03:46 +0600

    Details Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { css } from "@emotion/react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SyncLoader from "react-spinners/SyncLoader";
 import "./App.css";
 import About from "./Components/About";
@@ -9,6 +10,7 @@ import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Portfolio from "./Components/Portfolio";
+import PortfolioDetails from "./Components/PortfolioDetails";
 import Resume from "./Components/Resume";
 import Testimonials from "./Components/Testimonials";
 
@@ -44,25 +46,31 @@ const App = () => {
   }, [])
 
   return (
-    <>
+    <BrowserRouter>
       {loading ?
         <>
           <SyncLoader color={'#ef4444'} loading={loading} size={30} css={override} />
         </>
 
         :
-        <div className="App">
-          <Header data={resumeData.main} />
-          <About data={resumeData.main} />
-          <Resume data={resumeData.resume} />
-          <Portfolio data={resumeData.portfolio} />
-          <Testimonials data={resumeData.testimonials} />
-          <Contact data={resumeData.main} />
-          <Footer data={resumeData.main} />
-        </div>
-
+        <Switch>
+          <Route path="/:title">
+            <PortfolioDetails data={resumeData.portfolio}/>
+          </Route>
+          <Route path="/">
+            <div className="App">
+              <Header data={resumeData.main} />
+              <About data={resumeData.main} />
+              <Resume data={resumeData.resume} />
+              <Portfolio data={resumeData.portfolio} />
+              <Testimonials data={resumeData.testimonials} />
+              <Contact data={resumeData.main} />
+              <Footer data={resumeData.main} />
+            </div>
+          </Route>
+        </Switch>
       }
-    </>
+    </BrowserRouter>
 
   );
 };
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { Link } from 'react-router-dom';
 import "swiper/components/effect-coverflow/effect-coverflow.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import SwiperCore, {
@@ -15,24 +16,16 @@ import "swiper/swiper.min.css";
 SwiperCore.use([EffectCoverflow, Pagination]);
 
 
-const Portfolio = ({data}) => {
-  // const [allProjects, setAllProjects] = useState([]);
-  // //fetch 
-  // useEffect(() => {
-  //   axios.get('http://localhost:5000/projects')
-  //     .then(res => console.log(res.data))
-  // }, [])
-
-
+const Portfolio = ({ data }) => {
   if (data) {
     var projects = data.projects.map(function (project) {
-      var projectImage = "images/portfolio/" + project.image;
+      // var projectImage = "images/portfolio/" + project.image;
       return (
         <SwiperSlide>
           <div key={project.projectTitle} className=" portfolio-item card bg-white p-8 rounded-xl shadow-xl">
-            <div className="item-wrap rounded-xl shadow-xl" style={{height:'300px',overflow:'hidden'}}>
-              <img className="rounded-xl shadow-xl object-cover" alt={project.projectTitle} src={projectImage} />
-          </div>
+            <div className="item-wrap rounded-xl shadow-xl" style={{ height: '300px', overflow: 'hidden' }}>
+              <img className="rounded-xl shadow-xl object-cover" alt={project.projectTitle} src={project.images[0]} />
+            </div>
 
             <h2 className="text-gray-600 py-6 text-3xl">{project.projectTitle}</h2>
             <span>Technologies: {project.projectTools}</span>
@@ -43,14 +36,17 @@ const Portfolio = ({data}) => {
                   Live Demo
                 </button>
               </a>
-              <button className="bg-secondary px-5 py-3 rounded-lg shadow-xl my-7 text-white font-bold">
-                Details
-              </button>
+              <Link to={`/${project.projectTitle}`}>
+                <button className="bg-secondary px-5 py-3 rounded-lg shadow-xl my-7 text-white font-bold">
+                  Details
+                </button>
+              </Link>
+
               <a className="github_icon" href={project.github} target="_blank" rel="noreferrer">
                 <FaGithub className="text-5xl" />
               </a>
             </div>
-        </div>
+          </div>
         </SwiperSlide>
       );
     });
@@ -58,7 +54,7 @@ const Portfolio = ({data}) => {
 
   return (
     <section id="portfolio">
-      <div className="row" style={{maxWidth:'1400px'}}>
+      <div className="row" style={{ maxWidth: '1400px' }}>
         <div className="twelve columns collapsed" data-aos="fade-right" data-aos-duration="1500">
           <h1 className="text-center"><span>My Projects</span></h1>
 
@@ -66,56 +62,56 @@ const Portfolio = ({data}) => {
             id=""
             className=""
           > */}
-            <Swiper 
-              loop={true}
+          <Swiper
+            loop={false}
             autoplay={{
               delay: 3000,
               disableOnInteraction: false
             }}
-            effect={'coverflow'} 
-            grabCursor={true} 
-            centeredSlides={false} 
-            slidesPerView={3} 
-            coverflowEffect={{ "rotate": 0, "stretch": 0, "depth": 300, "modifier": 1, "slideShadows": false }} 
-            pagination={true} 
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={false}
+            slidesPerView={3}
+            coverflowEffect={{ "rotate": 0, "stretch": 0, "depth": 300, "modifier": 1, "slideShadows": false }}
+            pagination={true}
             className="mySwiper p-9 box-border"
-              breakpoints={{
-                320:{
-                  slidesPerView: 1,
-                },
-                375:{
-                  slidesPerView: 1,
-                },
-                425:{
-                  slidesPerView: 1,
-                },
-                500: {
-                  slidesPerView: 1,
-                },
-                768: {
-                  slidesPerView: 2,
-                },
-                1024: {
-                  slidesPerView: 3
-                },
-                1200: {
-                  slidesPerView: 3
-                },
-                1400: {
-                  slidesPerView: 3
-                }
-              }}
-            >
-                {projects}
-            </Swiper>
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+              },
+              375: {
+                slidesPerView: 1,
+              },
+              425: {
+                slidesPerView: 1,
+              },
+              500: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3
+              },
+              1200: {
+                slidesPerView: 3
+              },
+              1400: {
+                slidesPerView: 3
+              }
+            }}
+          >
+            {projects}
+          </Swiper>
 
           <div className="flex justify-center mt-4">
             <a href="http://github.com/19smabtahinoor" target="_blank" rel="noopener noreferrer" className="bg-primary px-6 py-3 rounded-full text-white flex items-center space-x-3" style={{ color: 'white' }}>
               Check More Projects <HiOutlineArrowNarrowRight />
             </a>
           </div>
-          </div>
         </div>
+      </div>
       {/* </div> */}
     </section>
   );
